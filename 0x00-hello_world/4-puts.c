92c5e3d7bd63e814233f5dcddf586ff55b22edcc
ChahrazadBouragba <chbouragba25@gmail.com>
2023-02-16 22:19:56 +0000

    putsc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A programme that prints a line with put function
  * Return: 0 (success)
  */
-init main(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
