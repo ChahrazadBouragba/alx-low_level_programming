b51d575328f744c2c430999737dc4c18bc859a24
ChahrazadBouragba <chbouragba25@gmail.com>
2023-02-23 07:19:41 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,18 @@
 #include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: Variable value
+ * Return: Returns 1 or 2
+ */
+int _isupper(int c)
+{
+	if (c>='A' && c<= 'B')
+	{
+		return (1);
+	}
+			else
+			{
+				return (0);
+			}
+}
